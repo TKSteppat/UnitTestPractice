8e358fe45fe93a3d434a65f0dc512f7580e21e82
Tyler Steppat <157649983+TKSteppat@users.noreply.github.com>
2025-11-05 11:23:47 -0800

    Update PasswordTest.cpp

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,31 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, multiple_leading_same_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("AAAxyz");
+    ASSERT_EQ(2, actual);
+}
+
+TEST(PasswordTest, mixed_leading_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("ABBBB");
+    ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, no_leading_characters)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, different_leading_character)
+{
+    Password my_password;
+    int actual = my_password.count_leading_characters("bbB");
+    ASSERT_EQ(0, actual);
+}
